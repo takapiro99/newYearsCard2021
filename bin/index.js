fe17e8dec86f803cb3f8fd92d577975415b02e2e
takapiro99 <bigstitch99@gmail.com>
2020-12-31 22:25:38 +0900

    made colorful one (cowsay->lolcat)
    全部の機能完成した
    
    よ

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,41 @@
 #!/usr/bin/env node
+const meow = require("meow");
+const chalk = require("chalk");
 
-require("../lib/index")();
+const newYearsCard = require("../lib/index");
+
+const cli = meow(
+  `
+  ${chalk.blueBright("Usage")}
+    $ newYearsCard <options>
+
+  ${chalk.blueBright("Options")}
+    -n, --name     Include your name
+
+  ${chalk.blueBright("Examples")}
+    $ newYearsCard
+    $ newYearsCard -n John
+`,
+  {
+    flags: {
+      name: {
+        type: "string",
+        alias: "n",
+      },
+      // secret option
+      random: {
+        type: "boolean",
+        alias: "r",
+      },
+    },
+  }
+);
+/*
+{
+	input: ['unicorns'],
+	flags: {rainbow: true},
+	...
+}
+*/
+
+newYearsCard(cli.input[0], cli.flags);
